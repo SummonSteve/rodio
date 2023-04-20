b3b05818ac6cdd2e0dad952480820896f9bf4346
MyAngelATRI <atrias@qq.com>
2023-04-20 13:54:37 +0800

    bump cpal version

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,36 +1,17 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+use rodio::OutputStream;
 use std::io::BufReader;
-use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let device = cpal::available_hosts();
 
-    let file = std::fs::File::open("assets/beep.wav").unwrap();
-    let beep1 = stream_handle.play_once(BufReader::new(file)).unwrap();
-    beep1.set_volume(0.2);
-    println!("Started beep1");
+    let host = cpal::host_from_id(cpal::HostId::Asio).unwrap();
+    let device = host.default_output_device().unwrap();
 
-    thread::sleep(Duration::from_millis(1500));
+    let (_stream, handle) = OutputStream::try_from_device(&device).unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+    let file = std::fs::File::open("assets/music.mp3").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
-    let file = std::fs::File::open("assets/beep2.wav").unwrap();
-    let beep2 = stream_handle.play_once(BufReader::new(file)).unwrap();
-    beep2.set_volume(0.3);
-    beep2.detach();
-    println!("Started beep2");
-
-    thread::sleep(Duration::from_millis(1500));
-    let file = std::fs::File::open("assets/beep3.ogg").unwrap();
-    let beep3 = stream_handle.play_once(file).unwrap();
-    beep3.set_volume(0.2);
-    println!("Started beep3");
-
-    thread::sleep(Duration::from_millis(1500));
-    drop(beep1);
-    println!("Stopped beep1");
-
-    thread::sleep(Duration::from_millis(1500));
-    drop(beep3);
-    println!("Stopped beep3");
-
-    thread::sleep(Duration::from_millis(1500));
+    sink.sleep_until_end();
 }
